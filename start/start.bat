6895ae058c0d9f53ad8cd2467769be1a97f3f90b
alexey.rosolko <alexey.rosolko@gmail.com>
2026-07-31 10:59:08 +0300

    docker compose up -d --pull always wh

--- start/start.bat
+++ start/start.bat
@@ -1,5 +1,6 @@
 git fetch --all
 git merge --all
+@REM docker compose down
 docker compose stop
 docker compose up -d --pull always wh
 docker compose start
--- start/start.sh
+++ start/start.sh
@@ -1,5 +1,6 @@
 git fetch --all
 git merge --all
-docker compose down
+#docker compose down
+docker compose stop
 docker compose up -d --pull always wh
 docker compose start
